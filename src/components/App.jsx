ce7fcd5eb79efea86b0378c21cf0070697058d72
Porter Savage <theportersavage@gmail.com>
2019-05-10 11:37:15 -0700

    added component tree picture to README.md

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <div>
-        <style>
-          {`
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      champions: [],
+      isLoaded: false,
+    };
+  }
 
-          `}
-        </style>
-      </div>
-      <div>
+  componentDidMount() {
+    fetch('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
+      .then(res => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          champions: json,
+        });
+      });
+  }
 
+  render() {
+    return (
+      <div>
+        <div>
+          <style>
+            {`
+  
+            `}
+          </style>
+        </div>
+        <div>
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
